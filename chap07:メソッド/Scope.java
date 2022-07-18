4f880ddcb07dab7e015d1e7ad6ec65c8e90d3025
shumpei-kayano <73801780+shumpei-kayano@users.noreply.github.com>
2022-07-18 11:10:08 +0900

    変数のスコープ

--- chap07:メソッド/Scope.java
+++ chap07:メソッド/Scope.java
@@ -2,21 +2,23 @@
 
 class Scope {
 
+	//複数のメソッドで共有する変数（フィールド）はstaticをつけて宣言する
 	static int x = 700;
 
+	//PrintXメソッド
 	static void printX() {
 		System.out.println("x = " + x);
 	}
 
 	public static void main(String[] args) {
-		System.out.println("x = " + x);
+		System.out.println("x = " + x); //staticのx 700が出力
 
-		int x = 800;
+		int x = 800; //局所変数 staticとは別のx
 
-		System.out.println("x = " + x);
+		System.out.println("x = " + x); //局所変数の 800が出力
 
-		System.out.println("Scope.x = " + Scope.x);
+		System.out.println("Scope.x = " + Scope.x); //staticのx 700が出力
 
-		printX();
+		printX(); //staticのx 700が出力
 	}
 }
