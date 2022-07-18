35474c2c06834d776dc2e5f73f7eb793ab313478
shumpei-kayano <73801780+shumpei-kayano@users.noreply.github.com>
2022-07-18 09:51:35 +0900

    乱数の生成 Random ランダム

--- chap02:変数・文字入力/LuckyNo.java
+++ chap02:変数・文字入力/LuckyNo.java
@@ -1,14 +1,26 @@
 // 0～9のラッキーナンバーを乱数で生成して表示
+// 乱数の生成
 
+//Randomクラスをimport
 import java.util.Random;
 
 class LuckyNo {
 
 	public static void main(String[] args) {
+		//Randomのインスタンスを生成
 		Random rand = new Random();
-
+		//メソッドを使用し、乱数生成
 		int lucky = rand.nextInt(10);			// 0～9の乱数
 
 		System.out.println("今日のラッキーナンバーは" + lucky + "です。");
 	}
 }
+
+/* Randomクラスのメソッド
+ * nextBoolean() true または false
+ * nextInt()
+ * nextInt(n) 0~n-1
+ * nextLong()
+ * nextDouble()
+ * nextFloat()
+ */
