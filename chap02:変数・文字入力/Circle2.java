baa9aca933cf24611b0addff5f51c4868b658360
shumpei-kayano <73801780+shumpei-kayano@users.noreply.github.com>
2022-07-18 09:48:21 +0900

    final変数　定数

--- chap02:変数・文字入力/Circle2.java
+++ chap02:変数・文字入力/Circle2.java
@@ -1,10 +1,13 @@
 // 円周の長さと円の面積を求める（その２：円周率をfinal変数で表す）
+// final変数で定数を宣言
+// final変数は初期化が原則
 
 import java.util.Scanner;
 
 class Circle2 {
 
 	public static void main(String[] args) {
+		//↓final変数
 		final double PI = 3.1416;			// 円周率
 		Scanner stdIn = new Scanner(System.in);
 
